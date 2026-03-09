265dfd467a611838b157e43089b90e061bfd29fc
Szabolcs Szasz <szasz.szabolcs1995@gmail.com>
2026-03-09 13:33:12 +0100

    fbevents

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
 
 import { getDictionary } from "../../dictionaries";
 import CookieBanner from "../../components/CookieBanner";
+import { FacebookPixel } from "../../components/FacebookPixel";
 
 import Script from "next/script";
 
@@ -22,36 +23,11 @@ export default async function RootLayout(props) {
   return (
     <html lang={lang}>
       <head>
-        <Script
-          id="fb-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1241080515923812');
-              fbq('track', 'PageView');
-            `,
-          }}
-        />
+        <FacebookPixel />
       </head>
       <body className={inter.className}>
         {children}
         <CookieBanner dict={dict} />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1241080515923812&ev=PageView&noscript=1"
-          />
-        </noscript>
       </body>
     </html>
   );
